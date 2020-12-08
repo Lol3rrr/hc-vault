b6f20db4fd862892faa309de5df6632842b04736
lol3rrr <s.loler03@gmail.com>
2020-12-08 09:20:44 +0100

    Added another test for kv2-update_set

--- tests/kv2/update_set.rs
+++ tests/kv2/update_set.rs
@@ -47,3 +47,52 @@ async fn valid_update_set_no_options() {
         }
     };
 }
+
+#[tokio::test]
+async fn valid_update_set_with_options() {
+    let mock_server = MockServer::start().await;
+
+    let client_token = "testToken";
+
+    let req_data = json!({
+        "testKey": "testValue"
+    });
+    let req_cas: u16 = 2;
+
+    let req_body = json!({
+        "data": req_data,
+        "options": {
+            "cas": 2,
+        },
+    });
+
+    Mock::given(method("POST"))
+        .and(path("/v1/kv/data/test"))
+        .and(header("X-Vault-Token", client_token))
+        .and(body_json(&req_body))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&mock_server)
+        .await;
+
+    let mut client =
+        match hc_vault::Client::new_token(mock_server.uri().clone(), client_token.to_string(), 120)
+            .await
+        {
+            Err(e) => {
+                assert!(false, "Should not return error: '{}'", e);
+                return;
+            }
+            Ok(s) => s,
+        };
+
+    match hc_vault::kv2::update_set(&mut client, "kv", "test", req_data.clone(), Some(req_cas))
+        .await
+    {
+        Err(e) => {
+            assert!(false, "Should not return error: '{}'", e);
+        }
+        Ok(_) => {
+            assert!(true);
+        }
+    };
+}
